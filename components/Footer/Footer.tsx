5f60f3554b07fa1958f84e9cf5b199bc0133d964
danielliucs <daniell.liu@mail.utoronto.ca>
2022-08-08 18:04:51 -0400

    basic structure for footer down

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
+import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <Link href='/'><a>Action</a></Link>
-            <Link href='/'><a>Action</a></Link>
-            <Link href='/'><a>Action</a></Link>
-        </div>
+        <div className= "wrapper">
+            <button className='button-style'><Link href='/'><a>Action</a></Link></button>
+            <button className='button-style'><Link href='/'><a>Action</a></Link></button>
+            <button className='button-style'><Link href='/'><a>Action</a></Link></button>
+        </div>  
     );
 }
 export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../styles/footerStyle.css';
+import '../styles/Home.module.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
