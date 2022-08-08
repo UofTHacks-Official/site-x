97a616292d4ee6a26828581601412c8993bcd377
danielliucs <daniell.liu@mail.utoronto.ca>
2022-08-08 16:19:58 -0400

    added some folders and typescript files

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <Link href='/'><a>Action</a></Link>
+            <Link href='/'><a>Action</a></Link>
+        </div>
+    );
+}
+export default Footer;
