66e8d662ae5b652ffcea7e62f3f9fec95fb93ca8
danielliucs <daniell.liu@mail.utoronto.ca>
2022-08-26 13:24:57 -0400

    made footer css file non global

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import React from 'react';
+import foot from '../../styles/Footer.module.css'
 
 const Footer = () => {
     return (
-        <div className= "wrapper">
+        <div className= {foot.wrapper}>
             <button className='button-style'><Link href='/'><a>Action</a></Link></button>
             <button className='button-style'><Link href='/'><a>Action</a></Link></button>
             <button className='large'><Link href='/'><a>Action</a></Link></button>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import '../styles/footerStyle.css';
+//import '../styles/footerStyle.css';
 import '../styles/Home.module.css';
 import '../styles/NavbarStyles.css';
 
