deb93cc23d6418630b92d1432b70b23cfa4a1431
ShernanJ <shernanjavier@gmail.com>
2022-11-25 20:07:21 -0500

    Adjusted vectors

--- components/atoms/Background.jsx
+++ components/atoms/Background.jsx
@@ -17,7 +17,6 @@ const Wrapper = styled("div", {
   variants: {
     bg: {
       main: {
-        background: image.lightsOnly
       },
       starsOnly: {
         background: '#0D0D0D',
@@ -30,7 +29,8 @@ const Wrapper = styled("div", {
 })
 
 const StarsWrapper = styled("div", {
-  background: image.starsOnly
+  background: image.starsOnly,
+  zIndex: "-1"
 })
 
 const Padding = styled("main", {
@@ -47,8 +47,54 @@ const ArtWrapper = styled("div", {
   width: "100%",
   pointerEvents: "none",
   backgroundSize: "cover",
-  backgroundImage: vectors.stroke1
+  backgroundImage: vectors.stroke1,
 })
+
+const TopLeftGlow = styled("div", {
+  position: 'fixed',
+  top: '-95vh',
+  left: '-450px',
+  height: '1200px',
+  width: '1200px',
+  background: 'radial-gradient(circle, rgba(180,135,238,1) 0%, rgba(180,135,238,1) 50%, rgba(180,135,238,0) 65%)',
+  opacity: '0.06',
+})
+
+const TopRightGlow = styled("div", {
+  position: 'fixed',
+  top: '-55vh',
+  left: '60vw',
+  right: '0px',
+  bottom: '0px',
+  height: '1200px',
+  width: '1200px',
+  background: 'radial-gradient(circle, rgba(94,167,213,1) 0%, rgba(94,167,213,1) 50%, rgba(0,212,255,0) 65%)',
+  opacity: '0.06',
+});
+
+const BottomLeftGlow = styled("div", {
+  position: 'fixed',
+  top: '60vh',
+  left: '-450px',
+  right: '0px',
+  bottom: '0px',
+  height: '1200px',
+  width: '1200px',
+  background: 'radial-gradient(circle, rgba(235,156,92,1) 0%, rgba(235,156,92,1) 50%, rgba(235,156,92,0) 65%)',
+  opacity: '0.06',
+});
+
+const BottomRightGlow = styled("div", {
+  position: 'fixed',
+  top: '60vh',
+  left: '50vw',
+  right: '0px',
+  bottom: '0px',
+  height: '1200px',
+  width: '1200px',
+  background: 'radial-gradient(circle, rgba(223,125,125,1) 0%, rgba(223,125,125,1) 50%, rgba(223,125,125,0) 65%)',
+  opacity: '0.06',
+});
 
 /**
  * 
@@ -59,6 +105,10 @@ export const MainBackground = (props) => {
   return (
     <StarsWrapper>
       <Wrapper>
+        <TopLeftGlow />
+        <TopRightGlow />
+        <BottomLeftGlow />
+        <BottomRightGlow />
         <ArtWrapper></ArtWrapper>
         {props.padded ? <Padding>{props.children}</Padding> : props.children}
       </Wrapper>
