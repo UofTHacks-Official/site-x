4a0668893ffdc32953eeb99936e406a291f91605
Anne Hu <46465219+annehu2@users.noreply.github.com>
2022-12-09 03:00:08 -0500

    clean up

--- components/molecules/Navbar.jsx
+++ components/molecules/Navbar.jsx
@@ -2,33 +2,33 @@ import { styled, css } from "~stitches";
 import { Link } from "@components/atoms";
 import Logo from '@assets/OfficialLogoBG.svg';
 import Image from 'next/image';
-import {BrowserView, MobileView} from 'react-device-detect';
+import { BrowserView, MobileView } from 'react-device-detect';
 import { Squash as Hamburger } from 'hamburger-react';
 
 const Wrapper = styled("navbar", css({
-    position: "fixed",
-    width: "100%",
-    boxSizing: "border-box",
-    height: "80px",
-    background: "rgb(13, 13, 13)",
-    zIndex:"10",
-    borderBottom:'1px solid',
-    borderImageSource: "linear-gradient(91.6deg, #DF7D7D 6.77%, #B487EE 57.81%, #5EA7D5 88.54%)",
-    borderImageSlice: 1,
+  position: "fixed",
+  width: "100%",
+  boxSizing: "border-box",
+  height: "80px",
+  background: "rgb(13, 13, 13)",
+  zIndex: "10",
+  borderBottom: '1px solid',
+  borderImageSource: "linear-gradient(91.6deg, #DF7D7D 6.77%, #B487EE 57.81%, #5EA7D5 88.54%)",
+  borderImageSlice: 1,
 }));
 
 const Links = styled("div", css({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    gap: "30px",
-    padding: "0px",
-    gap: "41px",
-    flexDirection: "row",
-    position: "absolute",
-    height: "24px",
-    right: "245px",
-    top: "28px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  gap: "30px",
+  padding: "0px",
+  gap: "41px",
+  flexDirection: "row",
+  position: "absolute",
+  height: "24px",
+  right: "245px",
+  top: "28px",
 }));
 
 const LinksMobile = styled("div", ({
@@ -36,7 +36,7 @@ const LinksMobile = styled("div", ({
   alignItems: "center",
   justifyContent: "center",
   flexDirection: "column",
-  width:"100%",
+  width: "100%",
   display: "none",
   gap: "10px",
   bottom: "20px",
@@ -44,18 +44,13 @@ const LinksMobile = styled("div", ({
 
 const LogoDiv = styled("div", ({
   position: 'absolute',
-  width: '80px',
-  height: '80px',
+  maxWidth: '80px',
+  maxHeight: '80px',
+  minWidth: '60px',
+  minHeight: '60px',
+  width: "20%",
   left: '10%',
   top: '0px',
-}));
-
-const LogoMobileDiv = styled("div", ({
-  position: 'absolute',
-  width: '60px',
-  height: '60px',
-  left: '10%',
-  top: '10px',
 }));
 
 const HamburgerWrapper = styled("div", ({
@@ -70,17 +65,17 @@ export const Navbar = (props) => {
       document.getElementById("navbar").style.height = '220px';
       document.getElementById("mobile-links").style.display = 'flex';
     } else {
-      document.getElementById("navbar").style.height  = '80px';
+      document.getElementById("navbar").style.height = '80px';
       document.getElementById("mobile-links").style.display = 'none';
     }
   }
 
   return (
     <Wrapper id="navbar">
-      <BrowserView>
       <LogoDiv>
         <Image src={Logo} alt='official logo'></Image>
       </LogoDiv>
+      <BrowserView>
         <Links id="links">
           <Link href="https://forms.gle/2AjFu2TzLVFSoDPo7">Mentor</Link>
           {/* <Link href="">Volunteer</Link> */}
@@ -88,16 +83,13 @@ export const Navbar = (props) => {
           <Link href="#Sponsors" target="_self">Sponsors</Link>
           <Link href="#FAQ" target="_self">FAQ</Link>
         </Links>
-        </BrowserView>
+      </BrowserView>
       <MobileView>
-      <LogoMobileDiv>
-        <Image src={Logo} alt='official logo'></Image>
-      </LogoMobileDiv>
         <HamburgerWrapper>
-          <Hamburger rounded size={30} zIndex="10000" onToggle={toggled => {changeNavBar(toggled);}} />
+          <Hamburger rounded size={30} zIndex="10000" onToggle={toggled => { changeNavBar(toggled); }} />
         </HamburgerWrapper>
         <LinksMobile id='mobile-links'>
-        <Link href="https://forms.gle/2AjFu2TzLVFSoDPo7">Mentor</Link>
+          <Link href="https://forms.gle/2AjFu2TzLVFSoDPo7">Mentor</Link>
           {/* <Link href="">Volunteer</Link> */}
           <Link href="#About" target="_self">About</Link>
           <Link href="#Sponsors" target="_self">Sponsors</Link>
