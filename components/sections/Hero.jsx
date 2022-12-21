378bb27a26d938940202b65f7b4d54cca6f2b0f9
Anne Hu <46465219+annehu2@users.noreply.github.com>
2022-12-20 22:48:32 -0500

    close hacker apps

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -22,8 +22,7 @@ export const Hero = (props) => {
         </span>
       </Subtitle>
       <DisplayText>UofTHacks X</DisplayText>
-      <Body size="large">Hacker applications are now open! <span style ={{fontSize: "1rem"}}>(Deadline Extended to Dec 23rd, 2022.)</span></Body>
-      <ApplyButton />
+      <Body size="large">Hacker applications are now closed. Thank you!<span style ={{fontSize: "1rem"}}></span></Body>
       <Body><a href='mailto:sponsors@uofthacks.com' style={{color: "#ffff", "textDecoration": "underline"}}>Interested in sponsoring?</a></Body>
     </Wrapper>
   )
