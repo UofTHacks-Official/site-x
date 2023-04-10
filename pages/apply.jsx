cfaab917a8ab22b4232e82c92aca8fcae041dc6d
Gunin Wasan <guninw1001@gmail.com>
2023-04-10 11:27:15 -0400

    mobile fix

--- pages/apply.jsx
+++ pages/apply.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function ViewRecruitmentPackage() {
   return (
-    <div style={{ height: "100vh" }}>
-      <embed src="UofTHacks11.pdf" width="100%" height="100%" type="application/pdf" />
+    <div style={{height: '100vh', overflow: 'hidden'}}>
+      <iframe src="UofTHacks11.pdf" style={{width: '100%', height: '100vh', border: 'none'}} scrolling="no" />
     </div>
   );
 }
