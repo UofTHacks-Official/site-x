95cfcd91beba674b2c8877a9efbf1a4d27eec623
Gunin Wasan <guninw1001@gmail.com>
2023-04-10 12:51:52 -0400

    mobile fix v3

--- pages/apply.jsx
+++ pages/apply.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
 
 function ViewRecruitmentPackage() {
+    const [width, setWidth] = React.useState(0);
+    const [height, setHeight] = React.useState(0);
+  
+    React.useEffect(() => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+      const handleResize = () => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+      };
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
   return (
-    <div style={{height: '100vh', overflow: 'hidden'}}>
-      <iframe src="UofTHacks11.pdf" style={{width: '100%', height: '100vh', border: 'none'}} scrolling="no" />
-    </div>
+    <embed
+      src="UofTHacks11.pdf"
+      width={width}
+      height={height}
+      type="application/pdf"
+    />
   );
 }
 
